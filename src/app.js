5931f04d74bd7dbca3180c0aadf5a95ac0af46e6
GUILHERME SILVEIRA VAZ <04626245013@senacrs.edu.br>
2026-06-11 14:47:05 -0300

    Aula 13  Integra front-end com API usando fetch

--- src/app.js
+++ src/app.js
@@ -1,13 +1,20 @@
 const express = require('express');
+const agendamentoRoutes = require('./routes/agendamentos.routes');
+const usuariosRoutes = require('./routes/usuarios.routes')
+const authRoute = require('./routes/auth.routes')
+
 
 const app = express();
-
 app.use(express.json());
+app.use(express.static('public'))
 
 
 app.get('/', (req, res) =>{
     res.send('API organizada funcionando')
 })
 
+app.use('/api', agendamentoRoutes);
+app.use('/api', usuariosRoutes)
+app.use('/api', authRoute)
 
 module.exports = app;
